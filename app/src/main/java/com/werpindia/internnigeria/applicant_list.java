19880182f099a839cc677aa1d6dcb01ba40daac4
goblet1662 <kunal.h@somaiya.edu>
2019-03-20 18:45:49 +0530

    Applicant List

--- app/src/main/java/com/werpindia/internnigeria/applicant_list.java
+++ app/src/main/java/com/werpindia/internnigeria/applicant_list.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +11,22 @@ import java.util.List;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class applicant_list extends Fragment {
     private static final String tag ="Applicant_list";
     List<applications> applicants;
+public  applicant_list()
+{
+    //requires empty consrtuctor
+
+}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Nullable
     @Override
@@ -27,10 +35,15 @@ public class applicant_list extends Fragment {
 
 
         applicants=new ArrayList<>();
-        applicants.add(new applications("Kunal Hirani","abc@123.com",R.drawable.abc));
-        applicants.add(new applications("abc123","abc@123.com",R.drawable.abc));
-        applicants.add(new applications("abc xyz","abc@123.com",R.drawable.abc));
-        RecyclerView myrv=(RecyclerView) getActivity().findViewById(R.id.rv1);
+        applicants.add(new applications("Adam Smith","smithad@gmail.com",R.drawable.adam));
+        applicants.add(new applications("Christa Jacob","jacobchirs@gmail.com",R.drawable.christa));
+        applicants.add(new applications("Kunal Hirani","kunal.hirani@gmail.com",R.drawable.abc));
+        applicants.add(new applications("Steve Martin","stevemartin12@gmail.com.",R.drawable.seteve));
+        applicants.add(new applications("Emiley Cook","emiley1999@gmail.com",R.drawable.emiley));
+        applicants.add(new applications("Om Shukla","om.shukla@gmail.com",R.drawable.abc));
+        applicants.add(new applications("Marc Lewis","om.shukla@gmail.com",R.drawable.adam));
+        RecyclerView myrv=view.findViewById(R.id.rv1);
+        myrv.setHasFixedSize(true);
         RecylerViewAdapter recylerViewAdapter=new RecylerViewAdapter(getActivity(),applicants);
         final LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         layoutManager.setOrientation(RecyclerView.VERTICAL);
